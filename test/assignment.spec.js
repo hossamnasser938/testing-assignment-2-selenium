34479d0effc97e43a1fed3bf5402bbe9e00ddd24
Hossam Abdelnaser <hossam.abdelnaser@myoncare.com>
2026-01-02 23:11:09 +0200

    build assignment 3 test cases

---
+++ test/assignment.spec.js
@@ -0,0 +1,76 @@
+const { Builder, Browser, By } = require("selenium-webdriver");
+const assert = require("assert");
+
+const CORRECT_USERNAME = "standard_user";
+const CORRECT_PASSWORD = "secret_sauce";
+const INCORRECT_PASSWORD = "wrong_pass";
+
+const SAUCE_DEMO_URL = "https://www.saucedemo.com";
+
+describe("login operations", () => {
+  let driver;
+
+  before(async function () {
+    driver = await new Builder().forBrowser(Browser.CHROME).build();
+    await driver.manage().setTimeouts({ implicit: 500 });
+  });
+
+  after(async () => await driver.quit());
+
+  it("successful login - correct credentials", async () => {
+    await driver.get(SAUCE_DEMO_URL);
+
+    const usernameInput = await driver.findElement(By.id("user-name"));
+    const paswordInput = await driver.findElement(By.id("password"));
+    const loginButton = await driver.findElement(By.id("login-button"));
+
+    await usernameInput.sendKeys(CORRECT_USERNAME);
+    await paswordInput.sendKeys(CORRECT_PASSWORD);
+    await loginButton.click();
+
+    const message = await driver.findElement(By.className("app_logo"));
+    const value = await message.getText();
+
+    assert.equal(value, "Swag Labs");
+  });
+
+  it("failed login - wrong credntials", async () => {
+    await driver.get(SAUCE_DEMO_URL);
+
+    const usernameInput = await driver.findElement(By.id("user-name"));
+    const paswordInput = await driver.findElement(By.id("password"));
+    const loginButton = await driver.findElement(By.id("login-button"));
+
+    await usernameInput.sendKeys(CORRECT_USERNAME);
+    await paswordInput.sendKeys(INCORRECT_PASSWORD);
+    await loginButton.click();
+
+    const message = await driver.findElement(By.css("h3"));
+    const value = await message.getText();
+
+    assert.equal(
+      value,
+      "Epic sadface: Username and password do not match any user in this service"
+    );
+  });
+
+  it("successful post login redirection - navigating to cart", async () => {
+    await driver.get(SAUCE_DEMO_URL);
+
+    const usernameInput = await driver.findElement(By.id("user-name"));
+    const paswordInput = await driver.findElement(By.id("password"));
+    const loginButton = await driver.findElement(By.id("login-button"));
+
+    await usernameInput.sendKeys(CORRECT_USERNAME);
+    await paswordInput.sendKeys(CORRECT_PASSWORD);
+    await loginButton.click();
+
+    const cartIcon = await driver.findElement(By.id("shopping_cart_container"));
+    await cartIcon.click();
+
+    const title = await driver.findElement(By.className("title"));
+    const value = await title.getText();
+
+    assert.equal(value, "Your Cart");
+  });
+});
